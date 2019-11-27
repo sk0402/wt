560d88db6875c2b436ec634c0c6448e23ed4a1ad
sk0402 <58185381+sk0402@users.noreply.github.com>
2019-11-27 15:16:23 +0530

    Create 9.php

---
+++ 9.php
@@ -0,0 +1,34 @@
+<?php
+Header(‘Content-Type:text/plain’);
+$allTheStates= “Mississippi Alabama Texas Massachusetts Kansas”;
+$statesArray=[];
+$states1=explode(‘ ’,$allTheStates);
+$i=0;
+//States that end in XAS
+foreach ($states1 as $state) {
+ if (preg_match('/^k.*s$/i', ($state))) {
+ $statesArray[$i] = ($state);
+ $i = $i + 1;
+ echo "\nThe states that begins with k ans ends in s:" . $state;
+ }
+ }
+//states that begins with M and ends in s
+ foreach($states1 as $state) {
+ if (preg_match('/^M.*s$/', ($state))) {
+ $statesArray[$i] = ($state);
+ $i = $i + 1; 
+echo "\nThe states that begins with M and ends in s:" . $state;
+ }
+ }
+//states that ends in a 
+foreach($states1 as $state) {
+if (preg_match('/a$/', ($state))){ 
+$statesArray[$i] = ($state);
+ $i = $i + 1;
+ echo "\nThe states that ends in a:" . $state;
+ }   
+}
+ foreach ($statesArray as $element => $value) {
+ print( "\n" . $value . " is the element " . $element); 
+}
+ ?>
