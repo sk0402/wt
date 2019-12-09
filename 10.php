b9ff495e4a9d47a6ea1211a614b8611940020ff0
sk0402 <58185381+sk0402@users.noreply.github.com>
2019-12-09 22:14:59 +0530

    Update 10.php

--- 10.php
+++ 10.php
@@ -1,80 +1,97 @@
-MySql -u root -p
-password : password
-show databases;
-create database sample10;
-use sample10;
-create table student(usn varchar(20),name varchar(20),address varchar(20));
-insert into student values('1AT16IS093','SANATH','BANGALORE');
-select * from student;
-
-
 <html>
- <body>
- <style>
- table, td, th 
+<body>
+<style>
+table, td, th
 {
-border: 1px solid black; 
+border: 1px solid black;
 width: 33%;
- text-align: center; 
+text-align: center;
 border-collapse:collapse;
- background-color:lightblue;
- } 
+background-color:lightblue;
+}
 table { margin: auto; }
- </style> 
+</style>
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = "root"; 
-$dbname = "sample"; 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "sample";
 $a=[];
- // Create connection // Opens a new connection to the MySQL server 
-$conn = mysqli_connect($servername, $username, $password, $dbname); 
-// Check connection and return an error description from the last connection error, if any 
+// Create connection // Opens a new connection to the MySQL server
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection and return an error description from the last connection error, if any
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error); $sql = "SELECT *
 FROM student";
- // performs a query against the database 
-$result = $conn->query($sql); 
-echo "<br>"; 
-echo "<center> BEFORE SORTING </center>"; 
+// performs a query against the database
+$result = $conn->query($sql);
+echo "<br>";
+echo "<center> BEFORE SORTING </center>";
 echo"<table border='2'>";
- echo"<tr>";
- echo "<th>usn</th><th>NAME</th><th>address</th></tr>";
- if ($result->num_rows> 0) 
-{ 
-// output data of each row and fetches a result row as an associative array 
+echo"<tr>";
+echo "<th>usn</th><th>NAME</th><th>address</th></tr>";
+if ($result->num_rows> 0)
+{
+// output data of each row and fetches a result row as an associative array
 while($row=$result->fetch_assoc())
-{ 
-echo "<tr>"; 
+{
+echo "<tr>";
 echo "<td>". $row["usn"]."</td>";
 echo"<td>".$row["name"]."</td>";
- echo "<td>". $row["address"]."</td></tr>"; 
+echo "<td>". $row["address"]."</td></tr>";
 array_push($a,$row["usn"]);
- }
-  {
- for($i=0;$i<$n;$i++)
- {
- if($row["usn"]== $a[$i])
- {
- $c[$i]=$row["name"];
- $d[$i]=$row["address"];
- }
- }
- }
- }
- echo "<br>";
- echo "<center> AFTER SORTING </center>";
- echo"<table border='2'>";
- echo"<tr>";
- echo"<th>usn</th><th>name</th><th>address</th></tr>";
- for($i=0;$i<$n;$i++)
- {
- echo "<tr>";
- echo"<td>".$a[$i]."</td>";
- echo "<td>". $c[$i]."</td>";
+}
+}
+else
+echo "Table is Empty";
+echo "</table>";
+$n=count($a);
+$b=$a;
+for ( $i = 0 ; $i< ($n - 1) ; $i++ )
+{
+$pos= $i;
+for ( $j = $i + 1 ; $j < $n ; $j++ )
+{
+if ( $a[$pos] > $a[$j] ) $pos= $j;
+}
+if ( $pos!= $i )
+{
+$temp=$a[$i];
+$a[$i] = $a[$pos];
+$a[$pos] = $temp;
+}
+}
+$c=[];
+$d=[];
+$result = $conn->query($sql);
+if ($result->num_rows> 0)
+// output data of each row
+{
+while($row=$result->fetch_assoc())
+{
+for($i=0;$i<$n;$i++)
+{
+if($row["usn"]== $a[$i])
+{
+$c[$i]=$row["name"];
+$d[$i]=$row["address"];
+}
+}
+}
+}
+echo "<br>";
+echo "<center> AFTER SORTING </center>";
+echo"<table border='2'>";
+echo"<tr>";
+echo"<th>usn</th><th>name</th><th>address</th></tr>";
+for($i=0;$i<$n;$i++)
+{
+echo "<tr>";
+echo"<td>".$a[$i]."</td>";
+echo "<td>". $c[$i]."</td>";
 echo "<td>". $d[$i]."</td></tr>";
- }
- echo "</table>";
+}
+echo "</table>";
 $conn->close();
- ?>
- </body>
- </html>
+?>
+</body>
+</html>
